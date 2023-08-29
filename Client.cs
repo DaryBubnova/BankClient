44422c7008e64b35ccf2f863bd1868f919b2f41c
Дарья Бубнова <non.iserere@gmail.com>
2023-08-29 17:14:23 +0700

    Добавьте файлы проекта.

---
+++ Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankClient
+{
+    class Client
+    {
+        private int _age;
+
+        public string Name { get; set; } = "John";
+        
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("Возраст не может быть отрицательным");
+
+                _age = value;
+            }
+        }   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            client.Age = 30;
+
+            BankAccount bankAccount = new BankAccount(client);
+
+            Console.WriteLine(bankAccount.CalculateExtraRate());
+        }
+    }
+}
